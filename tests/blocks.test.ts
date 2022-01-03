51d6bc3c52ee2df6f46bb67cedad13c11ec11857
Ben Keys <bkeys818@gmail.com>
2022-01-02 22:20:08 -0500

    🧪 Update tests

--- tests/blocks.test.ts
+++ tests/blocks.test.ts
@@ -1,7 +1,7 @@
-import type { Blocks } from '..'
+import type { Block } from '..'
 import richText from './rich-texts.test'
 
-const paragraph: Blocks.Paragraph = {
+const paragraph: Block<'paragraph'> = {
     object: 'block',
     id: '198012e8-57e4-4962-97f4-1979ba813464',
     created_time: '2021-12-31T15:23:00.000Z',
@@ -12,7 +12,7 @@ const paragraph: Blocks.Paragraph = {
     paragraph: { text: [richText] },
 }
 
-const bulletedListItem: Blocks.BulletedListItem = {
+const bulletedListItem: Block<'bulleted_list_item'> = {
     object: 'block',
     id: 'ff136785-29d6-4209-9756-1d5ccffcd6d0',
     created_time: '2021-12-31T15:25:00.000Z',
@@ -23,7 +23,7 @@ const bulletedListItem: Blocks.BulletedListItem = {
     bulleted_list_item: { text: [richText] },
 }
 
-const numberedListItem: Blocks.NumberedListItem = {
+const numberedListItem: Block<'numbered_list_item'> = {
     object: 'block',
     id: '4dd3d332-67f7-4610-ad3c-9764ebcfa31f',
     created_time: '2021-12-31T15:26:00.000Z',
@@ -34,7 +34,7 @@ const numberedListItem: Blocks.NumberedListItem = {
     numbered_list_item: { text: [richText] },
 }
 
-const toggle: Blocks.Toggle = {
+const toggle: Block<'toggle'> = {
     object: 'block',
     id: 'af4d4d4e-3bf8-494a-97a7-e62a88e9b1b1',
     created_time: '2021-12-31T15:27:00.000Z',
@@ -45,7 +45,7 @@ const toggle: Blocks.Toggle = {
     toggle: { text: [richText] },
 }
 
-const heading1: Blocks.Heading1 = {
+const heading1: Block<'heading_1'> = {
     object: 'block',
     id: '6fabc996-4f90-4ef6-b59a-2f2a5a5cd4b6',
     created_time: '2021-12-31T15:27:00.000Z',
@@ -56,7 +56,7 @@ const heading1: Blocks.Heading1 = {
     heading_1: { text: [richText] },
 }
 
-const heading2: Blocks.Heading2 = {
+const heading2: Block<'heading_2'> = {
     object: 'block',
     id: 'c1eb8f40-1f9c-42ca-bc71-5c945155ebc7',
     created_time: '2021-12-31T15:28:00.000Z',
@@ -67,7 +67,7 @@ const heading2: Blocks.Heading2 = {
     heading_2: { text: [richText] },
 }
 
-const heading3: Blocks.Heading3 = {
+const heading3: Block<'heading_3'> = {
     object: 'block',
     id: '56828d24-cdcf-4a1f-98ed-d8e2a9383662',
     created_time: '2021-12-31T15:29:00.000Z',
@@ -78,7 +78,7 @@ const heading3: Blocks.Heading3 = {
     heading_3: { text: [richText] },
 }
 
-const toDo: Blocks.ToDo = {
+const toDo: Block<'to_do'> = {
     object: 'block',
     id: 'dda38fb5-ef3f-4cac-bab9-2bc3f59d3b76',
     created_time: '2021-12-31T15:29:00.000Z',
@@ -89,7 +89,7 @@ const toDo: Blocks.ToDo = {
     to_do: { text: [richText], checked: false },
 }
 
-const quote: Blocks.Quote = {
+const quote: Block<'quote'> = {
     object: 'block',
     id: 'ed4dc4db-2c23-4c79-929f-b4c676c10c28',
     created_time: '2021-12-31T15:31:00.000Z',
@@ -100,7 +100,7 @@ const quote: Blocks.Quote = {
     quote: { text: [richText] },
 }
 
-const callout: Blocks.Callout = {
+const callout: Block<'callout'> = {
     object: 'block',
     id: 'e6d64b3f-f3fa-4d8b-a800-82cb9d46e73d',
     created_time: '2021-12-31T15:31:00.000Z',
@@ -111,7 +111,7 @@ const callout: Blocks.Callout = {
     callout: { text: [richText], icon: { type: 'emoji', emoji: '💡' } },
 }
 
-const syncedBlockSource: Blocks.SyncedBlock = {
+const syncedBlockSource: Block<'synced_block'> = {
     object: 'block',
     id: 'eca06c60-84c1-448c-b048-316d877ecd4d',
     created_time: '2021-12-31T15:32:00.000Z',
@@ -121,7 +121,7 @@ const syncedBlockSource: Blocks.SyncedBlock = {
     type: 'synced_block',
     synced_block: { synced_from: null },
 }
-const syncedBlock1: Blocks.SyncedBlock = {
+const syncedBlock1: Block<'synced_block'> = {
     object: 'block',
     id: '7f0c2ac4-b20f-4042-9acc-6a66b398cadc',
     created_time: '2021-12-31T15:32:00.000Z',
@@ -137,7 +137,7 @@ const syncedBlock1: Blocks.SyncedBlock = {
     },
 }
 
-const template: Blocks.Template = {
+const template: Block<'template'> = {
     object: 'block',
     id: 'b64de8b1-a276-406e-a530-d8d2f9231ce8',
     created_time: '2021-12-31T15:33:00.000Z',
@@ -148,7 +148,7 @@ const template: Blocks.Template = {
     template: { text: [richText] },
 }
 
-const columnList: Blocks.ColumnList = {
+const columnList: Block<'column_list'> = {
     object: 'block',
     id: 'dd50340b-2003-40b4-a565-5b622278d39d',
     created_time: '2021-12-31T15:34:00.000Z',
@@ -159,7 +159,7 @@ const columnList: Blocks.ColumnList = {
     column_list: {},
 }
 
-const childPage: Blocks.ChildPage = {
+const childPage: Block<'child_page'> = {
     object: 'block',
     id: '2ad92748-5fc3-4642-8f6e-a9937506db5e',
     created_time: '2021-12-31T15:35:00.000Z',
@@ -170,7 +170,7 @@ const childPage: Blocks.ChildPage = {
     child_page: { title: 'empty child page' },
 }
 
-const childDatabase: Blocks.ChildDatabase = {
+const childDatabase: Block<'child_database'> = {
     object: 'block',
     id: '1f06babd-1bb0-4c0b-9d45-d7370f48907f',
     created_time: '2021-12-31T15:36:00.000Z',
@@ -181,7 +181,7 @@ const childDatabase: Blocks.ChildDatabase = {
     child_database: { title: 'child database' },
 }
 
-const embed: Blocks.Embed = {
+const embed: Block<'embed'> = {
     object: 'block',
     id: 'c8bdf1ca-a117-4769-afca-e18e42d2c423',
     created_time: '2021-12-31T15:38:00.000Z',
@@ -195,7 +195,7 @@ const embed: Blocks.Embed = {
     },
 }
 
-const imageExternal: Blocks.Image = {
+const imageExternal: Block<'image'> = {
     object: 'block',
     id: '02d90ebf-8477-4476-9177-089c5a808f57',
     created_time: '2021-12-31T15:52:00.000Z',
@@ -209,7 +209,7 @@ const imageExternal: Blocks.Image = {
         external: { url: 'https://picsum.photos/300/200' },
     },
 }
-const imageInternal: Blocks.Image = {
+const imageInternal: Block<'image'> = {
     object: 'block',
     id: '3330703b-a5a0-4a1e-9452-d103041468b5',
     created_time: '2021-12-31T15:47:00.000Z',
@@ -227,7 +227,7 @@ const imageInternal: Blocks.Image = {
     },
 }
 
-const video: Blocks.Video = {
+const video: Block<'video'> = {
     object: 'block',
     id: '8e296761-aab2-4f91-a2a5-f1a81100aa4d',
     created_time: '2021-12-31T15:48:00.000Z',
@@ -244,7 +244,7 @@ const video: Blocks.Video = {
     },
 }
 
-const fileExternal: Blocks.File = {
+const fileExternal: Block<'file'> = {
     object: 'block',
     id: 'a6e7febe-6505-4042-994c-095fc693db28',
     created_time: '2021-12-31T15:54:00.000Z',
@@ -260,7 +260,7 @@ const fileExternal: Blocks.File = {
         },
     },
 }
-const fileInternal: Blocks.File = {
+const fileInternal: Block<'file'> = {
     object: 'block',
     id: '0ad691b1-bf12-4322-828f-2815f98da2be',
     created_time: '2021-12-31T15:49:00.000Z',
@@ -278,7 +278,7 @@ const fileInternal: Blocks.File = {
     },
 }
 
-const pdfExternal: Blocks.Pdf = {
+const pdfExternal: Block<'pdf'> = {
     object: 'block',
     id: '81c8d88b-0dcf-4d19-bf33-0354e167fc06',
     created_time: '2021-12-31T15:56:00.000Z',
@@ -294,7 +294,7 @@ const pdfExternal: Blocks.Pdf = {
         },
     },
 }
-const pdfInternal: Blocks.Pdf = {
+const pdfInternal: Block<'pdf'> = {
     object: 'block',
     id: '6be95125-899c-476e-8f15-b442bbd0134f',
     created_time: '2021-12-31T15:57:00.000Z',
@@ -312,7 +312,7 @@ const pdfInternal: Blocks.Pdf = {
     },
 }
 
-const bookmark: Blocks.Bookmark = {
+const bookmark: Block<'bookmark'> = {
     object: 'block',
     id: '83a81127-5dfa-4150-b2ed-693e7a3d3325',
     created_time: '2021-12-31T16:02:00.000Z',
@@ -326,7 +326,7 @@ const bookmark: Blocks.Bookmark = {
     },
 }
 
-const equation: Blocks.Equation = {
+const equation: Block<'equation'> = {
     object: 'block',
     id: '774e8443-c735-4bf7-a36d-ae7dad222f98',
     created_time: '2021-12-31T16:03:00.000Z',
@@ -340,7 +340,7 @@ const equation: Blocks.Equation = {
     },
 }
 
-const divider: Blocks.Divider = {
+const divider: Block<'divider'> = {
     object: 'block',
     id: '978611eb-a9e9-4cbd-a2c6-b05f03cb3970',
     created_time: '2021-12-31T16:06:00.000Z',
@@ -351,7 +351,7 @@ const divider: Blocks.Divider = {
     divider: {},
 }
 
-const tableOfContents: Blocks.TableOfContents = {
+const tableOfContents: Block<'table_of_contents'> = {
     object: 'block',
     id: '50a523f5-c25d-4617-ba0b-e803cf1f401e',
     created_time: '2021-12-31T16:06:00.000Z',
@@ -362,7 +362,7 @@ const tableOfContents: Blocks.TableOfContents = {
     table_of_contents: {},
 }
 
-const breadcrumb: Blocks.Breadcrumb = {
+const breadcrumb: Block<'breadcrumb'> = {
     object: 'block',
     id: '97776b04-57c7-45c9-ae2e-d6a21d8250e9',
     created_time: '2021-12-31T16:06:00.000Z',
@@ -373,7 +373,7 @@ const breadcrumb: Blocks.Breadcrumb = {
     breadcrumb: {},
 }
 
-const linkToPage: Blocks.LinkToPage = {
+const linkToPage: Block<'link_to_page'> = {
     object: 'block',
     id: '5ac69105-ed2f-447c-9e1e-22b0748fa5e3',
     created_time: '2021-12-31T16:08:00.000Z',
@@ -386,7 +386,7 @@ const linkToPage: Blocks.LinkToPage = {
         page_id: '003bd3fd-67e0-4c56-bca4-48bc7575f49e',
     },
 }
-const linkToDatabase: Blocks.LinkToPage = {
+const linkToDatabase: Block<'link_to_page'> = {
     object: 'block',
     id: 'c64291c3-4d36-45ae-b6c8-14a9f7bed2b8',
     created_time: '2021-12-31T16:08:00.000Z',
@@ -400,7 +400,7 @@ const linkToDatabase: Blocks.LinkToPage = {
     },
 }
 
-const linkPreview: Blocks.LinkPreview = {
+const linkPreview: Block<'link_preview'> = {
     object: 'block',
     id: '3154d54c-23ee-4c48-a7d4-c137c6be907f',
     created_time: '2021-12-31T16:18:00.000Z',
